2fcce6b78f3dbb540879c3ef6f0bcc69933253cc
RAHUL SINHA <35997582+Rahul1535@users.noreply.github.com>
2018-11-10 15:00:43 +0530

    login.js
    This is the javascript file

---
+++ login.js
@@ -0,0 +1,36 @@
+function log(){
+    var usr= document.getElementById("usr").value;
+    var pass= document.getElementById("pass").value;
+    if(usr=="davhc01" && pass=="hc01"){
+        alert(usr+"work under construction");
+    } else if(usr=="davhc02" && pass=="hc02"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc03" && pass=="hc03"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc04" && pass=="hc04"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc05" && pass=="hc05"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc06" && pass=="hc06"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc07" && pass=="hc07"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc08" && pass=="hc08"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc09" && pass=="hc09"){
+        alert(usr+"work under construction");
+    }
+    else if(usr=="davhc10" && pass=="hc10"){
+        alert(usr+"work under construction");
+    }
+    else{
+        alert("ERROR");
+    }
+}
